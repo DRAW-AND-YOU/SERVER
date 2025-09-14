3e830627da345b5c64d991338ae82463543017e5
unrookie21 <piresj0318@gmail.com>
2025-09-14 11:39:40 +0900

    FEAT #12 : API 공통 응답 클래스 구현

---
+++ src/main/java/com/drawandyou/drawandyou_server/global/common/response/ApiResponse.java
@@ -0,0 +1,28 @@
+package com.drawandyou.drawandyou_server.global.common.response;
+
+import org.springframework.http.HttpStatus;
+
+public record ApiResponse<T>(
+        boolean success,
+        int status,
+        String message,
+        T data
+) {
+    public static <T> ApiResponse<T> success(
+            HttpStatus httpStatus,
+            String message,
+            T data) {
+        return new ApiResponse<>(
+                true,
+                httpStatus.value(),
+                message,
+                data
+        );
+    }
+    public static ApiResponse<Void> success(
+            HttpStatus httpStatus,
+            String message
+    ) {
+        return success(httpStatus, message, null);
+    }
+}
--- src/main/java/com/drawandyou/drawandyou_server/user/presentation/UserController.java
+++ src/main/java/com/drawandyou/drawandyou_server/user/presentation/UserController.java
@@ -1,10 +1,19 @@
 package com.drawandyou.drawandyou_server.user.presentation;
 
+import com.drawandyou.drawandyou_server.global.common.response.ApiResponse;
+import com.drawandyou.drawandyou_server.user.presentation.message.ResponseMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
+
+    @GetMapping("/")
+    public ApiResponse<Void> test(){
+        return ApiResponse.success(HttpStatus.OK, ResponseMessage.TEST_SUCCESS.getMessage());
+    }
 
 }
