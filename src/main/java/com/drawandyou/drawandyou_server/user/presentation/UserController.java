2b5a835aeebaf91971f6588dfedb08bb00b369a9
unrookie21 <piresj0318@gmail.com>
2025-10-10 23:46:38 +0900

    CHORE #25 : UserController 에 RequestMapping 애노테이션 추각

--- src/main/java/com/drawandyou/drawandyou_server/user/presentation/UserController.java
+++ src/main/java/com/drawandyou/drawandyou_server/user/presentation/UserController.java
@@ -14,11 +14,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "User", description = "사용자 관련 API")
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
 
     private final UserService userService;
