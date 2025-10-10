22265488c54b67dafd345aa74af35082c0cde976
unrookie21 <piresj0318@gmail.com>
2025-10-10 23:38:44 +0900

    FEAT #25 : UserController 구현
    OAUTH 는 Spring Security 에서 일괄처리하므로 OauthController 가 별도로 필요하지 않습니다. UserController 에서는 일반 로그인 / 일반 회원가입 로직을 처리합니다.

--- src/main/java/com/drawandyou/drawandyou_server/user/presentation/UserController.java
+++ src/main/java/com/drawandyou/drawandyou_server/user/presentation/UserController.java
@@ -1,12 +1,19 @@
 package com.drawandyou.drawandyou_server.user.presentation;
 
+import com.drawandyou.drawandyou_server.global.auth.presentation.dto.UserAuthDto;
 import com.drawandyou.drawandyou_server.global.common.response.ApiResponse;
+import com.drawandyou.drawandyou_server.global.security.TokenProvider;
+import com.drawandyou.drawandyou_server.user.application.service.UserService;
+import com.drawandyou.drawandyou_server.user.domain.entity.User;
 import com.drawandyou.drawandyou_server.user.presentation.message.ResponseMessage;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "User", description = "사용자 관련 API")
@@ -14,10 +21,57 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-    @Operation(summary = "API 테스트", description = "API 서버 연결 상태를 확인하는 테스트 엔드포인트입니다.")
-    @GetMapping("/")
-    public ApiResponse<Void> test(){
-        return ApiResponse.success(HttpStatus.OK, ResponseMessage.TEST_SUCCESS.getMessage());
+    private final UserService userService;
+    private final TokenProvider tokenProvider;
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    /**
+     * 일반 로그인 - 회원가입
+     * @param userDTO
+     * @return
+     */
+    @Operation(summary = "일반 회원가입")
+    @PostMapping("/signup")
+    public ApiResponse<UserAuthDto> registerUser(@RequestBody UserAuthDto userDTO) {
+
+        if (userDTO == null || userDTO.password() == null) {
+            throw new RuntimeException("Invalid Password value.");
+        }
+
+        User user = User.builder()
+                .username(userDTO.username())
+                .password(passwordEncoder.encode(userDTO.password()))
+                .build();
+
+        User registeredUser = userService.create(user);
+        UserAuthDto responseUserDTO = UserAuthDto.builder()
+                .id(registeredUser.getId())
+                .username(registeredUser.getUsername())
+                .build();
+
+        return ApiResponse.success(HttpStatus.OK, ResponseMessage.USER_SIGNUP_SUCCESS.getMessage(), responseUserDTO);
     }
 
+    /**
+     * 일반 로그인 - 로그인
+     * @param userDTO
+     * @return
+     */
+    @Operation(summary = "일반 로그인")
+    @PostMapping("/signin")
+    public ApiResponse<UserAuthDto> authenticate(@RequestBody UserAuthDto userDTO) {
+        User user = userService.getByCredentials(userDTO.username(), userDTO.password(), passwordEncoder);
+
+        // 인증 성공시 jwt 토큰 발급
+        final String token = tokenProvider.create(user);
+
+        // 응답 객체에 사용자 정보 및 토큰 포함 (비밀번호 같은 민감 정보 포함 x)
+        final UserAuthDto responseUserDTO = UserAuthDto.builder()
+                .username(user.getUsername())
+                .id(user.getId())
+                .token(token)
+                .build();
+
+        return ApiResponse.success(HttpStatus.OK, ResponseMessage.USER_SIGNIN_SUCCESS.getMessage(), responseUserDTO);
+    }
 }
