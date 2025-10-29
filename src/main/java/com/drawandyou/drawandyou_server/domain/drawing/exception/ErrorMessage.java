e81f138bc2fd6b7c472f65ef48f98d7dcc5abd45
unrookie21 <piresj0318@gmail.com>
2025-10-30 02:27:02 +0900

    FEAT #41 : Drawing 도메인 예외 메시지 추가

---
+++ src/main/java/com/drawandyou/drawandyou_server/domain/drawing/exception/ErrorMessage.java
@@ -0,0 +1,9 @@
+package com.drawandyou.drawandyou_server.domain.drawing.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorMessage {
+}
