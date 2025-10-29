01880044cb85fa45f72960ea07b146111bd5b896
unrookie21 <piresj0318@gmail.com>
2025-10-29 23:24:46 +0900

    FIX #36 : 그림 분석 api 의 request 에 @RequestBody 애노테이션 추가

--- src/main/java/com/drawandyou/drawandyou_server/drawing/presentation/DrawingController.java
+++ src/main/java/com/drawandyou/drawandyou_server/drawing/presentation/DrawingController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,8 @@ public class DrawingController {
 
     @Operation(summary = "그림 저장 및 AI 분석결과 조회", description = "사용자가 그린 그림을 데이터 베이스에 저장하고 AI 분석 결과를 반환하는 API 입니다.")
     @PostMapping("/analyze")
-    public ApiResponse<DrawingAnalysisResponse> saveAndAnalyzeDrawing(@AuthenticationPrincipal Long userId, DrawingSaveRequest drawingSaveRequest){
+    public ApiResponse<DrawingAnalysisResponse> saveAndAnalyzeDrawing(@AuthenticationPrincipal Long userId,
+                                                                      @RequestBody DrawingSaveRequest drawingSaveRequest){
         DrawingAnalysisResponse response = drawingSaveService.saveAndAnalyzeDrawing(userId, drawingSaveRequest);
         return ApiResponse.success(HttpStatus.OK, ResponseMessage.DRAWING_ANALYSIS_SUCCESS.getMessage(), response);
     }
