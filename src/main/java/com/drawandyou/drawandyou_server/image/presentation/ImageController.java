35e0a460cd954c37d1d412982b865ea01dbe0efe
unrookie21 <piresj0318@gmail.com>
2025-10-29 21:58:10 +0900

    FEAT #36 : ImageController - getS3PresignedUrl 메소드 구현

--- src/main/java/com/drawandyou/drawandyou_server/image/presentation/ImageController.java
+++ src/main/java/com/drawandyou/drawandyou_server/image/presentation/ImageController.java
@@ -2,13 +2,14 @@ package com.drawandyou.drawandyou_server.image.presentation;
 
 import com.drawandyou.drawandyou_server.global.common.response.ApiResponse;
 import com.drawandyou.drawandyou_server.image.application.service.S3Service;
-import com.drawandyou.drawandyou_server.image.presentation.dto.response.ImageUploadResponse;
+import com.drawandyou.drawandyou_server.image.presentation.dto.request.PresignedUrlCreateRequest;
+import com.drawandyou.drawandyou_server.image.presentation.dto.response.PresignedUrlResponse;
+import com.drawandyou.drawandyou_server.image.presentation.message.ResponseMessage;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "Image", description = "이미지 업로드 관련 API")
 @RestController
@@ -18,22 +19,12 @@ public class ImageController {
 
     private final S3Service s3Service;
 
-    @Operation(summary = "이미지 업로드", description = "S3에 이미지를 업로드합니다.")
-    @PostMapping("/upload")
-    public ApiResponse<ImageUploadResponse> uploadImage(
-            @RequestParam("file") MultipartFile file,
-            @RequestParam(value = "folder", defaultValue = "images") String folder) {
-
-        String imageUrl = s3Service.uploadImage(file, folder);
-        ImageUploadResponse response = new ImageUploadResponse(imageUrl);
-
-        return ApiResponse.success(HttpStatus.OK, "이미지 업로드 성공", response);
+    @Operation(summary = "S3 Presigned URL 발급")
+    @PostMapping("/presigned-url")
+    public ApiResponse<PresignedUrlResponse> getS3PresignedUrl(PresignedUrlCreateRequest presignedUrlCreateRequest){
+        PresignedUrlResponse response = s3Service.createPresignedUrl(presignedUrlCreateRequest);
+        return ApiResponse.success(HttpStatus.OK, ResponseMessage.PRESIGNED_URL_CREATE_SUCCESS.getMessage(), response);
     }
 
-    @Operation(summary = "이미지 삭제", description = "S3에서 이미지를 삭제합니다.")
-    @DeleteMapping
-    public ApiResponse<Void> deleteImage(@RequestParam("url") String fileUrl) {
-        s3Service.deleteImage(fileUrl);
-        return ApiResponse.success(HttpStatus.OK, "이미지 삭제 성공");
-    }
+
 }
