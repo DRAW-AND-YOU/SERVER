af4cf742e7fc676e00141a8dd16e48b3f18f105a
unrookie21 <piresj0318@gmail.com>
2025-10-31 10:47:21 +0900

    FEAT #46 : FASTAPI 연동 테스트 진행

--- src/main/java/com/drawandyou/drawandyou_server/global/config/WebSecurityConfig.java
+++ src/main/java/com/drawandyou/drawandyou_server/global/config/WebSecurityConfig.java
@@ -60,6 +60,7 @@ public class WebSecurityConfig {
                         // Public endpoints (인증 불필요)
                         .requestMatchers(
                                 "/",
+                                "/api/test/**",
                                 "/api/auth/**",
                                 "/api/user/signup",
                                 "/api/user/signin",
