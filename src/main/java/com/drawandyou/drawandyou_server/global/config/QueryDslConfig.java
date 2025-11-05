f995295924e38356dea81913bbb8598ef77e59f0
unrookie21 <piresj0318@gmail.com>
2025-11-05 21:48:38 +0900

    FEAT #60 : QueryDSL 의존성 및 설정 클래스 추가

---
+++ src/main/java/com/drawandyou/drawandyou_server/global/config/QueryDslConfig.java
@@ -0,0 +1,16 @@
+package com.drawandyou.drawandyou_server.global.config;
+
+import com.querydsl.core.annotations.Config;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDslConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
+}
