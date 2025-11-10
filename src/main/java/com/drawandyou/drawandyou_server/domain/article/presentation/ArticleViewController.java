693b3ca80453eed95d57686fa68358463afa687b
unrookie21 <piresj0318@gmail.com>
2025-11-10 17:21:24 +0900

    REFACTOR #66 : Swagger 에 표시되는 api 이름 추가

--- src/main/java/com/drawandyou/drawandyou_server/domain/article/presentation/ArticleViewController.java
+++ src/main/java/com/drawandyou/drawandyou_server/domain/article/presentation/ArticleViewController.java
@@ -4,6 +4,7 @@ import com.drawandyou.drawandyou_server.domain.article.application.ArticleViewSe
 import com.drawandyou.drawandyou_server.domain.article.presentation.dto.response.ArticleViewCountResponse;
 import com.drawandyou.drawandyou_server.domain.article.presentation.message.ResponseMessage;
 import com.drawandyou.drawandyou_server.global.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,12 +19,14 @@ public class ArticleViewController {
 
     private final ArticleViewService articleViewService;
 
+    @Operation(summary = "게시글 조회수 증가")
     @PostMapping("/articles/{articleId}")
     public ApiResponse<ArticleViewCountResponse> increase(@PathVariable Long articleId){
         ArticleViewCountResponse response = articleViewService.increase(articleId);
         return ApiResponse.success(HttpStatus.OK, ResponseMessage.ARTICLE_VIEW_COUNT_INCREASEMENT_SUCCESS.getMessage(), response);
     }
 
+    @Operation(summary = "게시글 조회수 조회")
     @GetMapping("/articles/{articleId}/count")
     public ApiResponse<ArticleViewCountResponse> count(@PathVariable Long articleId){
         ArticleViewCountResponse response = articleViewService.count(articleId);
