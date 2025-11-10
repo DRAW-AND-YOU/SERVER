19f4b172eae443897ca580a21ac90b39e4815985
unrookie21 <piresj0318@gmail.com>
2025-11-10 17:15:15 +0900

    FEAT #66 : redis 에서 조회수 증가 및 조회를 담당하는 repository 클래스 구현

---
+++ src/main/java/com/drawandyou/drawandyou_server/domain/article/domain/repository/ArticleViewCountRepository.java
@@ -0,0 +1,28 @@
+package com.drawandyou.drawandyou_server.domain.article.domain.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ArticleViewCountRepository {
+
+    private final StringRedisTemplate redisTemplate;
+
+    // view:article::{article_id}::view_count
+    private static final String KEY_FORMAT = "view:article:%d:view_count";
+
+    public Long read(Long articleId){
+        String result = redisTemplate.opsForValue().get(generateKey(articleId));
+        return result == null ? 0l : Long.parseLong(result);
+    }
+
+    public Long increase(Long articleId){
+        return redisTemplate.opsForValue().increment(generateKey(articleId));
+    }
+    private String generateKey(Long articleId){
+        return String.format(KEY_FORMAT, articleId);
+    }
+
+}
