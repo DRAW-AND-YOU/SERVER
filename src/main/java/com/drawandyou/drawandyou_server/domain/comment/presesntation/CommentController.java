3de23f8226919da076854a995b11cc7f6e4f79ba
unrookie21 <piresj0318@gmail.com>
2025-11-14 17:03:39 +0900

    REFACTOR #71 : SWAGGER API 문서화

--- src/main/java/com/drawandyou/drawandyou_server/domain/comment/presesntation/CommentController.java
+++ src/main/java/com/drawandyou/drawandyou_server/domain/comment/presesntation/CommentController.java
@@ -6,9 +6,11 @@ import com.drawandyou.drawandyou_server.domain.comment.presesntation.request.Com
 import com.drawandyou.drawandyou_server.domain.comment.presesntation.response.CommentPageResponse;
 import com.drawandyou.drawandyou_server.domain.comment.presesntation.response.CommentResponse;
 import com.drawandyou.drawandyou_server.global.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "COMMENT", description = "댓글 관련 API")
@@ -19,26 +21,34 @@ public class CommentController {
 
     private final CommentService commentService;
 
+    @Operation(summary = "댓글 단건 조회")
     @GetMapping("/{commentId}")
     public ApiResponse<CommentResponse> read(@PathVariable Long commentId){
         CommentResponse response = commentService.read(commentId);
         return ApiResponse.success(HttpStatus.OK, ResponseMessage.COMMENT_READ_SUCCESS.getMessage(), response);
     }
 
+    @Operation(summary = "댓글 작성하기")
     @PostMapping
-    public ApiResponse<CommentResponse> create(@RequestBody CommentCreateRequest request){
-        CommentResponse response = commentService.create(request);
+    public ApiResponse<CommentResponse> create(
+            @AuthenticationPrincipal Long userId,
+            @RequestBody CommentCreateRequest request){
+        CommentResponse response = commentService.create(userId, request);
         return ApiResponse.success(HttpStatus.OK, ResponseMessage.COMMENT_CREATE_SUCCESS.getMessage(), response);
     }
 
+    @Operation(summary = "댓글 삭제하기")
     @DeleteMapping("{commentId}")
-    public ApiResponse<Void> delete(@PathVariable Long commentId){
-        commentService.delete(commentId);
+    public ApiResponse<Void> delete(
+            @AuthenticationPrincipal Long userId,
+            @PathVariable Long commentId){
+        commentService.delete(userId, commentId);
         return ApiResponse.success(HttpStatus.OK, ResponseMessage.COMMENT_DELETE_SUCCESS.getMessage());
     }
 
+    @Operation(summary = "댓글 목록 페이지네이션 조회")
     @GetMapping
-    public ApiResponse<CommentPageResponse >readAll(
+    public ApiResponse<CommentPageResponse> readAll(
             @RequestParam Long articleId,
             @RequestParam Long page,
             @RequestParam Long size){
