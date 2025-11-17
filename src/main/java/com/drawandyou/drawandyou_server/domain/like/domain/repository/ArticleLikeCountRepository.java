dcf8bc8e452e899b64d3983103277ed4b42b2653
unrookie21 <piresj0318@gmail.com>
2025-11-17 13:59:08 +0900

    REFACTOR #73 : 사용하지 않는 메소드 삭제

--- src/main/java/com/drawandyou/drawandyou_server/domain/like/domain/repository/ArticleLikeCountRepository.java
+++ src/main/java/com/drawandyou/drawandyou_server/domain/like/domain/repository/ArticleLikeCountRepository.java
@@ -1,21 +1,13 @@
 package com.drawandyou.drawandyou_server.domain.like.domain.repository;
 
 import com.drawandyou.drawandyou_server.domain.like.domain.entity.ArticleLikeCount;
-import jakarta.persistence.LockModeType;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
-
 public interface ArticleLikeCountRepository extends JpaRepository<ArticleLikeCount, Long> {
-
-    // select ... for update -> 읽기 시점 부터 쓰기락을 잡는 방법
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    Optional<ArticleLikeCount> findLockedByArticleId(Long articleId);
-
 
     @Query(
             value = "update article_like_count set like_count = like_count + 1 " +
