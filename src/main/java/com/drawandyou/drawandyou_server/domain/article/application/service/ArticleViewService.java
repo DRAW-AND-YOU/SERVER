d9582b1ef3d570b82319418f7872823e36e54901
unrookie21 <piresj0318@gmail.com>
2025-11-21 13:59:12 +0900

    REFACTOR #85 : ArticleViewService 에서 increase, count 메소드의 반환 타입 변경

--- src/main/java/com/drawandyou/drawandyou_server/domain/article/application/service/ArticleViewService.java
+++ src/main/java/com/drawandyou/drawandyou_server/domain/article/application/service/ArticleViewService.java
@@ -2,7 +2,6 @@ package com.drawandyou.drawandyou_server.domain.article.application.service;
 
 import com.drawandyou.drawandyou_server.domain.article.domain.repository.ArticleViewCountRepository;
 import com.drawandyou.drawandyou_server.domain.article.domain.repository.ArticleViewLockRepository;
-import com.drawandyou.drawandyou_server.domain.article.presentation.dto.response.ArticleViewCountResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,23 +18,21 @@ public class ArticleViewService {
     private static final int BACK_UP_BATCH_SIZE = 5;
     private static final Duration TTL = Duration.ofMinutes(10);
 
-    public ArticleViewCountResponse increase(Long articleId, Long userId){
+    public Long increase(Long articleId, Long userId){
 
         // LOCK 획득 실패시
         if (!articleViewLockRepository.lock(articleId, userId, TTL)){
-            Long count = articleViewCountRepository.read(articleId);
-            return new ArticleViewCountResponse(count);
-
+            return articleViewCountRepository.read(articleId);
         }
         Long count = articleViewCountRepository.increase(articleId);
         if (count % BACK_UP_BATCH_SIZE == 0){
             articleViewCountBackUpProcessor.backUp(articleId, count);
         }
-        return new ArticleViewCountResponse(count);
+        return count;
     }
 
-    public ArticleViewCountResponse count(Long articleId){
-        Long count = articleViewCountRepository.read(articleId);
-        return new ArticleViewCountResponse(count);
+    public Long count(Long articleId){
+        return articleViewCountRepository.read(articleId);
+
     }
 }
