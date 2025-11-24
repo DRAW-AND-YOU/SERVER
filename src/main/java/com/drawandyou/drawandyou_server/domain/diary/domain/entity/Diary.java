20238177855714daf8aea7973a88ec9066ddb047
unrookie21 <piresj0318@gmail.com>
2025-11-24 13:53:51 +0900

    FEAT #93 : Diary Entity 설계 및 인덱스 설정

---
+++ src/main/java/com/drawandyou/drawandyou_server/domain/diary/domain/entity/Diary.java
@@ -0,0 +1,53 @@
+package com.drawandyou.drawandyou_server.domain.diary.domain.entity;
+
+import com.drawandyou.drawandyou_server.domain.diary.domain.entity.enums.EmotionKeyword;
+import com.drawandyou.drawandyou_server.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(indexes = {
+        @Index(name = "idx_diary_author_written", columnList = "author_id , written_at")
+})
+public class Diary extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "diary_id")
+    private Long id;
+
+    // 일기 작성자 id
+    private Long authorId;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    // 일기 이미지 URL
+    private String imageUrl;
+
+    // 일기 작성 시간
+    private LocalDateTime writtenAt;
+
+    // 일기 작성 시점에서, 입력하는 감정 키워드
+    @Enumerated(value = EnumType.STRING)
+    private EmotionKeyword emotionKeyword;
+
+    public static Diary create(Long authorId, String title, String content,  String imageUrl, LocalDateTime writtenAt, EmotionKeyword emotionKeyword) {
+        return Diary.builder()
+                .authorId(authorId)
+                .title(title)
+                .content(content)
+                .imageUrl(imageUrl)
+                .writtenAt(writtenAt)
+                .emotionKeyword(emotionKeyword)
+                .build();
+    }
+
+}
