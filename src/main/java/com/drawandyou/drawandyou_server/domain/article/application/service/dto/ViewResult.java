1070ddd18d5139ff3e9346396489b54d0b685f1e
unrookie21 <piresj0318@gmail.com>
2025-12-04 16:10:40 +0900

    REFACTOR #113 : incremented 필드의 자료형을 Boolean -> boolean 으로 변경
    incremented 자료형은 null 값을 가질 가능성이 없으므로, 기본 타입으로 변경하였습니다.

--- src/main/java/com/drawandyou/drawandyou_server/domain/article/application/service/dto/ViewResult.java
+++ src/main/java/com/drawandyou/drawandyou_server/domain/article/application/service/dto/ViewResult.java
@@ -2,6 +2,6 @@ package com.drawandyou.drawandyou_server.domain.article.application.service.dto;
 
 public record ViewResult(
         Long count,
-        Boolean incremented
+        boolean incremented
 ) {
 }
