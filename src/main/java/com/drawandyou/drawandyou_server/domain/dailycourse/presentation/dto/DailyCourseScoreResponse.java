fc9456d9b1c734e8d1743c5706e39df7099c315c
unrookie21 <piresj0318@gmail.com>
2025-12-09 15:19:11 +0900

    Revert "REFACTOR #117 : 치유 프로그램 점수 변화 조회 api 응답 수정"
    This reverts commit 5542dcd6c8ebf1860ed78388af27d352ada911ab.

--- src/main/java/com/drawandyou/drawandyou_server/domain/dailycourse/presentation/dto/DailyCourseScoreResponse.java
+++ src/main/java/com/drawandyou/drawandyou_server/domain/dailycourse/presentation/dto/DailyCourseScoreResponse.java
@@ -2,9 +2,7 @@ package com.drawandyou.drawandyou_server.domain.dailycourse.presentation.dto;
 
 public record DailyCourseScoreResponse(
         Long dailyCourseId,
-        Long drawingAnalysisId,
         String imageUrl,
-        String title,
         Integer round,
         Integer score
 ) {
